dead5c469a389983bcba81cbe8a7fdbca73d10fb
Nick Chiconas <NIck.Chiconas@gmail.com>
2017-10-03 21:50:06 -0500

    Uploaded solution & files

---
+++ RREF/Program.cs
@@ -0,0 +1,253 @@
+﻿#define DEBUG
+//#undef DEBUG
+
+using System;
+using System.Diagnostics;
+
+namespace RREF
+{
+    class Program
+    {
+        private static double[,] matrix;
+        private static double[,] gaussReducedMatrix;
+        private static double[,] backReducedMatrix;
+        private static double[,] gaussJordanReducedMatrix;
+        private static bool test;
+        private static bool fail;
+        private static bool gaussBack;
+        private static bool bothMethods;
+
+        static void Main(string[] args)
+        {
+            // Default values loaded
+            test = false;
+            fail = false;
+            bothMethods = false;
+            gaussBack = true;
+
+            // Get user values
+#if DEBUG
+            FindOutIfTest();
+#endif
+            if (test)
+                LoadTestMatrix();
+            else
+                ParseMatrix();
+            FindOutIfGaussBack();
+            Console.WriteLine();
+
+            // Actually perform reduction, print out stats about the reduction
+            if (!fail)
+            {
+                
+                if (gaussBack)
+                {
+                    GaussBackReduce();
+                }
+                else
+                {
+                    if (!bothMethods)
+                    {
+                        GaussJordanReduce();
+                    }
+                    else
+                    {
+                        GaussBackReduce();
+                        Console.WriteLine();
+                        GaussJordanReduce();
+                    }
+                }
+                
+            }
+
+            // End execution
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void FindOutIfTest()
+        {
+            Console.Write("Is this a test run? (y/n): ");
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Y:
+                        test = true;
+                        Console.WriteLine();
+                        return;
+                    case ConsoleKey.N:
+                        test = false;
+                        Console.WriteLine();
+                        return;
+                    default:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                }
+            }
+        }
+
+        static void FindOutIfGaussBack()
+        {
+            Console.Write("Which method should be used: [G]auss Reduction + Back Reduction, Gauss-[J]ordan Reduction, or [B]oth? (g/j/b): ");
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.G:
+                        gaussBack = true;
+                        Console.WriteLine();
+                        return;
+                    case ConsoleKey.J:
+                        gaussBack = false;
+                        Console.WriteLine();
+                        return;
+                    case ConsoleKey.B:
+                        bothMethods = true;
+                        gaussBack = false;
+                        return;
+                    default:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                }
+            }
+        }
+
+        static void ParseMatrix()
+        {
+            // Get rows and columns of the matrix
+            Console.Write("Enter the number of rows in the matrix: ");
+            string rowsIn = Console.ReadLine();
+            if (!int.TryParse(rowsIn, out int rows))
+            {
+                Console.WriteLine("Error: Expected integer value. Exiting...");
+                fail = true;
+                return;
+            }
+            Console.Write("Enter the number of columns in the matrix: ");
+            string colsIn = Console.ReadLine();
+            if (!int.TryParse(colsIn, out int cols))
+            {
+                Console.WriteLine("Error: Expected integer value. Exiting...");
+                fail = true;
+                return;
+            }
+
+            // Initialize matrix, ask for each entry
+            matrix = new double[rows, cols];
+
+            Console.WriteLine(@"
+NOTE: all entries will be converted to double if not inputted as a double. Do not try to put in fractions, please convert them to a double value.
+
+NOTE 2: the ordered pair for the entry is not 0-based, for easy reading. (1,1) corresponds to the first entry of the first row of the matrix.
+");
+
+            string rawEntry;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"Entry at ({i + 1}, {j + 1}): ");
+                    rawEntry = Console.ReadLine();
+                    if (!double.TryParse(rawEntry, out double entry))
+                    {
+                        Console.WriteLine("Error: Expected double value. Exiting...");
+                        fail = true;
+                        return;
+                    }
+                    matrix[i, j] = entry;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Matrix input complete. Here is the entered matrix:");
+
+            // Print out matrix for confirmation
+            PrintMatrix(matrix);
+            Console.WriteLine();
+        }
+
+        static void LoadTestMatrix()
+        {
+            // This matrix is known to have an RREF that is the 3x3 identitiy matrix
+            matrix = new double[3, 3] 
+            {
+                {-1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            Console.WriteLine("Loaded default testing matrix. Default matrix:");
+            PrintMatrix(matrix);
+            Console.WriteLine();
+        }
+
+        static void PrintMatrix(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    string num = matrix[i, j].ToString("0.000");
+                    if ((matrix[i, j] % 1) != 0)
+                    {
+                        Console.Write(String.Format("{0,6}", num));
+                    }
+                    else
+                    {
+                        Console.Write(String.Format("{0,6}", matrix[i, j]));
+                    }
+
+                    // Check to not print commas on last column of a row
+                    if (j != matrix.GetLength(1) - 1)
+                        Console.Write(", ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+        }
+
+        static void GaussBackReduce()
+        {
+            int totalOperations = 0;
+            Stopwatch timer;
+
+            Console.WriteLine("Using Gauss Reduction followed by Back Reduction...");
+
+            timer = Stopwatch.StartNew();
+            gaussReducedMatrix = RREF.GaussReduction(matrix);
+            timer.Stop();
+            totalOperations += RREF.NumRowOps;
+            Console.WriteLine("REF of matrix:");
+            PrintMatrix(gaussReducedMatrix);
+            Console.WriteLine();
+
+            timer.Start();
+            backReducedMatrix = RREF.BackReduction(gaussReducedMatrix);
+            timer.Stop();
+            totalOperations += RREF.NumRowOps;
+            Console.WriteLine("RREF of matrix:");
+            PrintMatrix(backReducedMatrix);
+            Console.WriteLine();
+
+            Console.WriteLine($"Time elapsed for Gauss & Back reduction: {timer.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Gauss & Back reduction took {totalOperations} row operation(s)");
+        }
+
+        static void GaussJordanReduce()
+        {
+            Stopwatch timer;
+
+            Console.WriteLine("Using Gauss-Jordan Reduction...");
+
+            timer = Stopwatch.StartNew();
+            gaussJordanReducedMatrix = RREF.GaussJordanReduction(matrix);
+            timer.Stop();
+            Console.WriteLine("RREF of matrix:");
+            PrintMatrix(gaussJordanReducedMatrix);
+            Console.WriteLine();
+
+            Console.WriteLine($"Time elapsed for Gauss-Jordan reduction: {timer.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Gauss-Jordan reduction took {RREF.NumRowOps} row operation(s)");
+        }
+    }
+}
---
+++ RREF/RREF.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RREF
+{
+    class RREF
+    {
+        public static int NumRowOps { get; set; }
+        private static double[,] matrix;
+        private static double[] tempRow;
+        private static int leadingEntry;
+
+        public static double[,] GaussReduction(double[,] inputMatrix)
+        {
+            // Set the matrix variable using the matrix that is passed in
+            matrix = inputMatrix;
+            NumRowOps = 0;
+
+            // If its a row vector, we're done
+            if (matrix.GetLength(0) == 1)
+                return matrix;
+
+            // Getting our first leading entry
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (matrix[0, i] == 0)
+                {
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                    {
+                        if (matrix[j, i] != 0)
+                        {
+                            RowExchange(0, j);
+                            break;
+                        }
+                    }
+
+                    if (matrix[0, i] != 0)
+                    {
+                        leadingEntry = i;
+                        break;
+                    }
+                }
+            }
+            // Delete all non-zero entries below the leading entry
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, leadingEntry] != 0)
+                {
+                    RowTransvection(i, 0, matrix[i, leadingEntry] / matrix[0, leadingEntry]);
+                }
+            }
+
+            // Go through remaining rows, get leading entries, continue putting into REF
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                // Check if its a 0 row, if so then swap with below and execute the check again
+                if ((row != matrix.GetLength(0) - 1) && IsZeroRow(row))
+                {
+                    for (int i = row + 1; i < matrix.GetLength(0); i++)
+                    {
+                        if (!IsZeroRow(i))
+                        {
+                            RowExchange(row, i);
+                            break;
+                        }
+                    }
+                }
+
+                // Get leading entry
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] != 0)
+                    {
+                        leadingEntry = col;
+                        break;
+                    }
+                }
+
+                // Zero out all entries below leading entry
+                for (int i = row + 1; i < matrix.GetLength(0); i++)
+                {
+                    if (matrix[i, leadingEntry] != 0)
+                    {
+                        RowTransvection(i, row, matrix[i, leadingEntry] / matrix[row, leadingEntry]);
+                    }
+                }
+            }
+
+            // Return the strict Gauss reduced matrix
+            return matrix;
+        }
+
+        public static double[,] BackReduction(double[,] inputMatrix)
+        {
+            // Set the matrix variable using the matrix that is passed in
+            matrix = inputMatrix;
+            NumRowOps = 0;
+
+            // If its a row vector, we're done
+            if (matrix.GetLength(0) == 1)
+                return matrix;
+
+            // Go through each row, get leading entry (if its not a zero row), normalize leading entry, zero out entries above it
+            for (int row = matrix.GetLength(0) - 1; row > 0; row--)
+            {
+                // If the current row is a zero row, skip this iteration and move up a row.
+                if (IsZeroRow(row))
+                {
+                    continue;
+                }
+
+                // Find leading entry of the current row
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] != 0)
+                    {
+                        leadingEntry = col;
+                        break;
+                    }
+                }
+
+                // Check if leading entry is 1. If not, do row op to make it 1
+                if (matrix[row, leadingEntry] != 1)
+                {
+                    RowMultiplication(row, 1 / matrix[row, leadingEntry]);
+                }
+
+                // Go through each row above current row, zero out each non-zero entry
+                for (int i = row - 1; i >= 0; i--)
+                {
+                    if (matrix[i, leadingEntry] != 0)
+                    {
+                        RowTransvection(i, row, matrix[i, leadingEntry]);
+                    }
+                }
+            }
+
+            // Normalize the first row
+            if (matrix[0, 0] != 1)
+            {
+                RowMultiplication(0, 1 / matrix[0, 0]);
+            }
+
+            // Return the strict back reduced matrix
+            return matrix;
+        }
+
+        public static double[,] GaussJordanReduction(double[,] inputMatrix)
+        {
+            matrix = inputMatrix;
+            NumRowOps = 0;
+
+            // TODO: Implement
+
+            return matrix;
+        }
+
+        public static void RowTransvection(int row1, int row2, double factor)
+        {
+            //Ri -> Ri - kRj
+            tempRow = new double[matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[row1, i] -= (factor * matrix[row2, i]);
+            }
+            NumRowOps++;
+        }
+
+        public static void RowMultiplication(int row, double factor)
+        {
+            // Ri -> kRi
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[row, i] *= factor;
+            }
+            NumRowOps++;
+        }
+
+        public static void RowExchange(int row1, int row2)
+        {
+            // Ri <-> Rj
+            tempRow = new double[matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                tempRow[i] = matrix[row1, i];
+                matrix[row1, i] = matrix[row2, i];
+                matrix[row2, i] = tempRow[i];
+            }
+            NumRowOps++;
+        }
+
+        public static bool IsZeroRow(int row)
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (matrix[row, i] != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
